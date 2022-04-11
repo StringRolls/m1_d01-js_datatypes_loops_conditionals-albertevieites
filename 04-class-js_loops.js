bd5ec1e838e789731cfa6eae6784908cf3581f1e
Alberte Vieites <albertevieites@tutanota.com>
2022-04-11 18:31:05 +0200

    add: fill the loop file

--- 04-class-js_loops.js
+++ 04-class-js_loops.js
@@ -1,1 +1,17 @@
 // Class code examples
+// for
+const text = "Fuffy";
+
+for (let i = 0; i < text.length; i++) {
+  console.log(text.charAt(i));
+}
+
+// while
+let inputValue = 11;
+console.log("Input a value from 10 to 100");
+
+while (inputValue < 10) {
+  value = prompt("Input a value greater than 10...");
+}
+
+console.log("Finally, dude! Your input is ${inputValue}");
