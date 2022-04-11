a2c084263d241974aab75d90d1bd4c541c0111b1
Alberte Vieites <albertevieites@tutanota.com>
2022-04-11 18:48:55 +0200

    add: datatype file filled

--- 01-class-js_datatypes_operands.js
+++ 01-class-js_datatypes_operands.js
@@ -96,23 +96,3 @@ console.log(str.includes("to be")); // true
 console.log(str.includes("question")); // true
 console.log(str.includes("nonexistent")); // false
 console.log(str.includes("To be", 1)); // false
-
-// booleans are fun!
-// Logical operators: AND
-console.log(true && true); // => true
-console.log(true && false); // => false
-console.log(false && true); // => false
-console.log(false && false); // => false
-
-// Logical operators: OR
-console.log(true || true); // => true
-console.log(true || false); // => true
-console.log(false || true); // => true
-console.log(false || false); // => false
-
-// Boolean operators do not return boolenas
-console.log("cappuccino" && 4 > 2);
-
-var firstName;
-
-console.log(`Hola ${firstName || "guest"}`);
