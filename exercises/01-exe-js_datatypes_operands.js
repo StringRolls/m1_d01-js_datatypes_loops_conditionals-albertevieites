a9b26ce399b660a0336c8842889ebb4b626abde9
Alberte Vieites <albertevieites@tutanota.com>
2022-04-12 02:00:34 +0200

    add: 01 exe completed

--- exercises/01-exe-js_datatypes_operands.js
+++ exercises/01-exe-js_datatypes_operands.js
@@ -1,15 +1,22 @@
 // Practice JS datatypes and operands
 
 // 1. Create variables with your first name, last name and age.
-
+let firstName = "alberte";
+let lastName = "vieites";
+let age = 44;
 // 2. Make the first letters of your first and last name capitalized.
+let capFirstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+console.log(capFirstName);
 
+let capLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+console.log(capLastName);
 // 3. Concatenate your first and last name (after capitalizing them) to give your full name.
-
-// other: 
+let fullName = capFirstName + ' ' + lastName;
+// other:
 // let fullName = capFirstName + ' ' + capLastName;
 
 // 4. Use console.log() method to output the sentence with the following structure: `My name is __________ and I am  _____ years old.`
+console.log(`My name is ${fullName} and I am ${age} years old.`)
 
 // All primitive data types are immutable - meaning, once a primitives are created, they can't be modified.
 // Example from lesson:
@@ -25,10 +32,10 @@ city[0] = "B";
 console.log(city); // ==> ??
 
 const message = "Don't be sad, be happy!";
-console.log(message.slice(0,3, "don"));
+console.log(message.slice(0, 3, "don"));
 console.log(message); // ==> ??
 
-const statement = 'I love JavaScript!';
+const statement = "I love JavaScript!";
 const subStatement = statement.substr(2, 9);
 console.log(statement);
 
