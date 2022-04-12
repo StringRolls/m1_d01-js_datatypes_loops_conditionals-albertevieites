6b72a2f9327cfb599609a95f1b58756e179b9caa
Alberte Vieites <albertevieites@tutanota.com>
2022-04-12 02:50:15 +0200

    add: 04 exe completed

--- exercises/04-exe-js_loops.js
+++ exercises/04-exe-js_loops.js
@@ -4,8 +4,24 @@
   // replace each dot and comma with space. The final output should be: hello dear friend! nice to see you!.
   
   let str = 'hello,dear.friend! nice,to.see you!'
-  
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "," || str[i] === ".") {
+      const regex = /[.,]/g;
+      str = str.replace(regex, " ");
+    }
+  }
+    
 // 2. Loop from 0 to 15 with the conditions:
 // For every number that is divisible by 3 output ‘Fizz’ in the console
 // For every number that is divisible by 5 output ‘Buzz’ in the console
 // For every number that is divisible by 3 and 5 output ‘FizzBuzz’ in the console
+for (let i = 0; i <= 15; i++) {
+  if ([i] % 3 === 0) {
+    console.log("Fizz");
+  } else if ([i] % 5 === 0) {
+    console.log("Buzz");
+  } else if ([i] % 3 === 0 && [i] % 5 === 0) {
+    console.log("FizzBuzz")
+  }
+}
